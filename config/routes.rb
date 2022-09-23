1ff1b881dc5ccd60d2b3f65baa8fdce5405334d4
Superman128913 <statony86@gmail.com>
2022-09-23 13:43:01 -0400

    Finsih layout and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
-  get 'static_pages/home'
-  get 'static_pages/help'
+  get '/home', to:'static_pages#home'
+  get '/help',to: 'static_pages#help'
+  get '/about', to: 'static_pages#about'
+  get '/signup', to: 'users#new'
+
   resources :custom_travels
   resources :users
-  root 'users#index'
+
+  root 'static_pages#home'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
